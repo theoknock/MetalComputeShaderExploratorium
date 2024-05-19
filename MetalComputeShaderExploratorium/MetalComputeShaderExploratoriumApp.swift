229142e70f6504cfdd98b537421e581f630bdcd4
James Alan Bush <jamesalanbush1972@gmail.com>
2024-05-19 16:34:31 -0400

    Initial Commit

---
+++ MetalComputeShaderExploratorium/MetalComputeShaderExploratoriumApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalComputeShaderExploratoriumApp.swift
+//  MetalComputeShaderExploratorium
+//
+//  Created by Xcode Developer on 5/19/24.
+//
+
+import SwiftUI
+
+@main
+struct MetalComputeShaderExploratoriumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
